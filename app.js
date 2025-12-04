264fd6e9f84699422489301f8c66c0cb8bc6dd8f
jboschlagos <jboschlagos@gmail.com>
2025-12-03 22:34:32 -0300

    correcciones finales para que aparezca en html

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-window.alert("A continuación ingresa tus notas para calcular su promedio");
+alert("A continuación ingresa tus notas para calcular su promedio");
 
-var nota1 = parseFloat(window.prompt("Ingrese nota 1"));
-var nota2 = parseFloat(window.prompt("Ingrese nota 2"));
-var nota3 = parseFloat(window.prompt("Ingrese nota 3"));
-var nota4 = parseFloat(window.prompt("Ingrese nota 4"));
+var nota1 = parseFloat(prompt("Ingrese nota 1"));
+var nota2 = parseFloat(prompt("Ingrese nota 2"));
+var nota3 = parseFloat(prompt("Ingrese nota 3"));
+var nota4 = parseFloat(prompt("Ingrese nota 4"));
 
 var promedio = (nota1 + nota2 + nota3 + nota4) / 4;
 
-if (promedio >= 6 && promedio <= 7) {
-    const mensaje = "Excelente!! Tienes promedio " + promedio.toFixed(2);
-    const resultado = document.getElementById("resultado");
+const resultado = document.getElementById("resultado");
 
-    resultado.textContent = mensaje;
-    resultado.style.color = "green"; 
-    resultado.style.backgroundColor = "yellow";  
-}
- else if (promedio < 6 && promedio >= 5) {
-    document.write("Tienes promedio " + promedio.toFixed(1) + ". Sigue adelante, puedes mejorar");
+if (promedio >= 6 && promedio <= 7) {
+    resultado.textContent = `Excelente!! Tienes promedio ${promedio.toFixed(2)}`;
+    resultado.style.color = "green";
+    resultado.style.background = "yellow";
+
+} else if (promedio < 6 && promedio >= 5) {
+    resultado.textContent = `Tienes promedio ${promedio.toFixed(1)}. Sigue adelante, puedes mejorar`;
+    resultado.style.color = "blue";
+
 } else if (promedio < 5 && promedio >= 4) {
-    alert("Tienes promedio " + promedio.toFixed(1) + ". Esfuérzate más");
-} else if (promedio < 3.9 && promedio >= 1) {
-    console.log("Tienes promedio " + promedio.toFixed(1) + ". Lamentablemente has perdido el año, debes repetir");
+    resultado.textContent = `Tienes promedio ${promedio.toFixed(1)}. Esfuérzate más`;
+    resultado.style.color = "orange";
+    console.log("Necesitas mejorar tus notas");
+
+} else if (promedio < 4 && promedio >= 1) {
+    resultado.textContent = `Tienes promedio ${promedio.toFixed(1)}. Lamentablemente has perdido el año`;
+    resultado.style.color = "red";
+    alert("Debes repetir el año escolar");
+
 } else {
-    alert("Valor no válido");
+    resultado.textContent = "Valor no válido";
+    resultado.style.color = "black";
+    resultado.style.background = "none";
 }
